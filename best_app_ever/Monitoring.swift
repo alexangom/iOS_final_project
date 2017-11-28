42a06f7c5fddc9171fbfdbdd1d00d21f43eb202d
Jinglin Liu <liujinglinlele@hotmail.com>
2017-11-27 17:36:50 -0700

    updateQuestionTVC

--- best_app_ever/Monitoring.swift
+++ best_app_ever/Monitoring.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+class Question {
+    var questionStr: String?
+    var answers: [String]?
+    var selectedAnswerIndex: Int?
+    
+    init(questionStr: String, answers: [String], selectedAnswerIndex: Int?) {
+        self.questionStr = questionStr
+        self.answers = answers
+        self.selectedAnswerIndex = selectedAnswerIndex
+    }
+    
+    func getquestionStr() -> String {
+        return questionStr!
+    }
+    
+    func getAnswer() -> [String] {
+        return answers!
+    }
+    
+    func getIndex() -> Int {
+        return selectedAnswerIndex!
+    }
+}
+
